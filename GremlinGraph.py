03e533eb28ca8c62c225a79fc3b3dbe6c38fafc1
petjohansson <191493+piizei@users.noreply.github.com>
2024-02-16 21:58:45 +0100

    code cleanup

--- GremlinGraph.py
+++ GremlinGraph.py
@@ -1,21 +1,23 @@
-# This is not part of LangChain at the moment, but I will contribute it there.
 import hashlib
 import sys
 from typing import Any, Dict, List, Optional
 
-from langchain_community.graphs.graph_document import GraphDocument
-from langchain_community.graphs.graph_store import GraphStore
 from langchain_core.utils import get_from_env
 
+from langchain_community.graphs.graph_document import GraphDocument, Node, Relationship
+from langchain_community.graphs.graph_store import GraphStore
 
 class GremlinGraph(GraphStore):
     """Gremlin wrapper for graph operations.
     Parameters:
     url (Optional[str]): The URL of the Gremlin database server or env GREMLIN_URI
-    username (Optional[str]): The collection-identifier like '/dbs/database/colls/graph' for the database or env GREMLIN_USERNAME
-    password (Optional[str]): The connection-key for database authentication or env GREMLIN_PASSWORD
+    username (Optional[str]): The collection-identifier like '/dbs/database/colls/graph'
+                               or env GREMLIN_USERNAME if none provided
+    password (Optional[str]): The connection-key for database authentication
+                              or env GREMLIN_PASSWORD if none provided
     traversal_source (str): The traversal source to use for queries. Defaults to 'g'.
-    message_serializer (Optional[Any]): The message serializer to use for requests. Defaults to serializer.GraphSONSerializersV2d0().
+    message_serializer (Optional[Any]): The message serializer to use for requests.
+                                        Defaults to serializer.GraphSONSerializersV2d0()
     *Security note*: Make sure that the database connection uses credentials
         that are narrowly-scoped to only include necessary permissions.
         Failure to do so may result in data corruption or loss, since the calling
@@ -26,33 +28,51 @@ class GremlinGraph(GraphStore):
         limit the permissions granted to the credentials used with this tool.
 
         See https://python.langchain.com/docs/security for more information.
+
+    *Implementation detais*:
+        The Gremlin queries are designed to work with Azure CosmosDB limitations
     """
 
     @property
     def get_structured_schema(self) -> Dict[str, Any]:
         pass
 
-    def __init__(self, url: Optional[str] = None, username: Optional[str] = None, password: Optional[str] = None,
-                 traversal_source: str = "g", message_serializer: Optional[Any] = None, ) -> None:
+    def __init__(
+        self,
+        url: Optional[str] = None,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        traversal_source: str = "g",
+        message_serializer: Optional[Any] = None,
+    ) -> None:
         """Create a new Gremlin graph wrapper instance."""
         try:
-            from gremlin_python.driver import client, serializer
             import asyncio
+
+            from gremlin_python.driver import client, serializer
+
             if sys.platform == "win32":
                 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
         except ImportError:
-            raise ValueError("Please install gremlin-python first: "
-                             "`pip3 install gremlinpython")
+            raise ValueError(
+                "Please install gremlin-python first: " "`pip3 install gremlinpython"
+            )
 
-        self.client = client.Client(url=get_from_env("url", "GREMLIN_URI", url), traversal_source=traversal_source,
-                                    username=get_from_env("username", "GREMLIN_USERNAME", username),
-                                    password=get_from_env("password", "GREMLIN_PASSWORD", password),
-                                    message_serializer=message_serializer if message_serializer else serializer.GraphSONSerializersV2d0(), )
+        self.client = client.Client(
+            url=get_from_env("url", "GREMLIN_URI", url),
+            traversal_source=traversal_source,
+            username=get_from_env("username", "GREMLIN_USERNAME", username),
+            password=get_from_env("password", "GREMLIN_PASSWORD", password),
+            message_serializer=message_serializer
+            if message_serializer
+            else serializer.GraphSONSerializersV2d0(),
+        )
+        self.schema: str = ""
 
     @property
     def get_schema(self) -> str:
         """Returns the schema of the Gremlin database"""
-        if self.schema is None:
+        if len(self.schema) == 0:
             self.refresh_schema()
         return self.schema
 
@@ -61,83 +81,102 @@ class GremlinGraph(GraphStore):
         Refreshes the Gremlin graph schema information.
         """
         vertex_schema = self.client.submit("g.V().label().dedup()").all().result()
-        edge_schema = self.client.submit("g.E().label().dedup()").all().result()
-
-        # Todo: Add relationship properties
-
-        self.schema = ("\n".join(
-            ["Node labes are the following:", ",".join(vertex_schema), "Edge labes are the following:",
-             ",".join(edge_schema)]))
+        edge_schema = self.client.submit("g.E().label().dedup()").all().result()        
+        self.schema = "\n".join(
+            [
+                "Node labes are the following:",
+                ",".join(vertex_schema),
+                "Edge labes are the following:",
+                ",".join(edge_schema),
+            ]
+        )
 
     def query(self, query: str) -> List[Dict[str, Any]]:
         q = self.client.submit(query)
-        return q.all().result()
+        a = q.all()
+        return a.result()
 
-    def add_graph_documents(self, graph_documents: List[GraphDocument], include_source: bool = False) -> None:
+    def add_graph_documents(
+        self, graph_documents: List[GraphDocument], include_source: bool = False
+    ) -> None:
         """
         Take GraphDocument as input as uses it to construct a graph.
         """
         node_cache = {}
         for document in graph_documents:
-
             if include_source:
                 # Create document vertex
-                doc_props = {'page_content': document.source.page_content, 'metadata': document.source.metadata}
+                doc_props = {
+                    "page_content": document.source.page_content,
+                    "metadata": document.source.metadata,
+                }
                 doc_id = hashlib.md5(document.source.page_content.encode()).hexdigest()
-                doc_node = self.add_node('Document', doc_id, doc_props, node_cache)
+                doc_node = self.add_node("Document", doc_id, doc_props, node_cache)
 
             # Import nodes to vertexes
             for el in document.nodes:
-                node = self.add_node(el.type, el.id, el.properties)
+                node = self.add_node(el)
                 if include_source:
                     # Add Edge to document for each node
-                    self.add_edge('source', doc_node, node, {})
-                    self.add_edge('document', node, doc_node, {})
+                    self.add_edge("source", doc_node, node, {})
+                    self.add_edge("document", node, doc_node, {})
 
             # Edges
             for el in document.relationships:
                 # Find or create the source vertex
-                source = self.add_node(el.source.type, el.source.id, el.source.properties, node_cache)
+                self.add_node(
+                    el.source, node_cache
+                )
                 # Find or create the target vertex
-                target = self.add_node(el.target.type, el.target.id, el.target.properties, node_cache)
+                self.add_node(
+                    el.target, node_cache
+                )
                 # Find or create the edge
-                self.add_edge(el.type, source, target, el.properties)
+                self.add_edge(el)
 
-    def build_vertex_query(self, label_value: str, id_value, properties: dict):
-        base_query = f"g.V().hasLabel('{label_value}').has('id','{id_value}').fold().coalesce(unfold(),addV('{label_value}').property('id','{id_value}').property('type', '{label_value}')"
-        for key, value in properties.items():
+    def build_vertex_query(self, node: Node):                        
+        base_query = (
+            f"g.V().has('id','{node.id}').fold()"
+            + f".coalesce(unfold(),addV('{node.type}')"
+            + f".property('id','{node.id}')"            
+            + f".property('type','{node.type}')"
+        )
+        for key, value in node.properties.items():
             base_query += f".property('{key}', '{value}')"
 
         return base_query + ")"
 
-    def build_edge_query(self, type: str, source_node: dict, target_node: dict, properties: dict):
-        source_query = f".hasLabel('{source_node['label']}').has('type', '{source_node['label']}').has('id','{source_node['id']}')"
-        target_query = f".hasLabel('{target_node['label']}').has('type', '{target_node['label']}').has('id','{target_node['id']}')"
-        source = f"g.V()" + source_query
-        target = f"g.V()" + target_query
+    def build_edge_query(
+        self, relationship: Relationship
+    ):
+        source_query = f".has('id','{relationship.source.id}')"        
+        target_query = f".has('id','{relationship.target.id}')"        
 
-        base_query = f"""{source}.as('a')  
+        base_query = f""""g.V(){source_query}.as('a')  
             .V(){target_query}.as('b') 
             .choose(
-                __.inE('{type}').where(outV().as('a')),
+                __.inE('{relationship.type}').where(outV().as('a')),
                 __.identity(),
-                __.addE('{type}').from('a').to('b')
+                __.addE('{relationship.type}').from('a').to('b')
             )        
-            """.replace('\n', '').replace('\t', '')
-        for key, value in properties.items():
+            """.replace("\n", "").replace("\t", "")
+        for key, value in relationship.properties.items():
             base_query += f".property('{key}', '{value}')"
 
         return base_query
 
-    def add_node(self, type: str, id: str, properties: dict, node_cache: dict = {}):
-        if id in node_cache:
-            return node_cache[id]
+    def add_node(self, node: Node, node_cache: dict = {}):
+        # if properties does not have label, add type as label
+        if "label" not in node.properties:
+            node.properties["label"] = node.type        
+        if node.id in node_cache:
+            return node_cache[node.id]
         else:
-            query = self.build_vertex_query(type, id, properties)
-            node = self.client.submit(query).all().result()[0]
-            node_cache[id] = node
+            query = self.build_vertex_query(node)
+            _ = self.client.submit(query).all().result()[0]
+            node_cache[node.id] = node
             return node
 
-    def add_edge(self, type: str, source: dict, target: dict, properties: dict):
-        query = self.build_edge_query(type, source, target, properties)
+    def add_edge(self, relationship: Relationship):
+        query = self.build_edge_query(relationship)
         return self.client.submit(query).all().result()[0]
--- gremlinqa.py
+++ gremlinqa.py
@@ -3,19 +3,20 @@ from __future__ import annotations
 
 from typing import Any, Dict, List, Optional
 
-from langchain_core.callbacks import CallbackManagerForChainRun
+from GremlinGraph import GremlinGraph
+from langchain_core.callbacks.manager import CallbackManager, CallbackManagerForChainRun
 from langchain_core.language_models import BaseLanguageModel
 from langchain_core.prompts import BasePromptTemplate
+from langchain_core.prompts.prompt import PromptTemplate
 from langchain_core.pydantic_v1 import Field
 
 from langchain.chains.base import Chain
 from langchain.chains.graph_qa.prompts import (
     CYPHER_QA_PROMPT,
+    GRAPHDB_SPARQL_FIX_TEMPLATE,
     GREMLIN_GENERATION_PROMPT,
 )
 from langchain.chains.llm import LLMChain
-
-from GremlinGraph import GremlinGraph
 
 INTERMEDIATE_STEPS_KEY = "intermediate_steps"
 
@@ -29,7 +30,7 @@ def extract_gremlin(text: str) -> str:
     Returns:
         Gremlin code extracted from the text.
     """
-    return text.replace('`', '').removeprefix("gremlin").replace('\n', '')
+    return text.replace("`", "").removeprefix("gremlin").replace("\n", "")
 
 
 class GremlinQAChain(Chain):
@@ -50,6 +51,8 @@ class GremlinQAChain(Chain):
     graph: GremlinGraph = Field(exclude=True)
     gremlin_generation_chain: LLMChain
     qa_chain: LLMChain
+    gremlin_fix_chain: LLMChain
+    max_fix_retries: int = 3
     input_key: str = "query"  #: :meta private:
     output_key: str = "result"  #: :meta private:
     top_k: int = 100
@@ -75,27 +78,34 @@ class GremlinQAChain(Chain):
 
     @classmethod
     def from_llm(
-            cls,
-            llm: BaseLanguageModel,
-            *,
-            qa_prompt: BasePromptTemplate = CYPHER_QA_PROMPT,
-            gremlin_prompt: BasePromptTemplate = GREMLIN_GENERATION_PROMPT,
-            **kwargs: Any,
+        cls,
+        llm: BaseLanguageModel,
+        *,
+        gremlin_fix_prompt: BasePromptTemplate = PromptTemplate(
+            input_variables=["error_message", "generated_sparql", "schema"],
+            template=GRAPHDB_SPARQL_FIX_TEMPLATE.replace("SPARQL", "Gremlin").replace(
+                "in Turtle format", ""
+            ),
+        ),
+        qa_prompt: BasePromptTemplate = CYPHER_QA_PROMPT,
+        gremlin_prompt: BasePromptTemplate = GREMLIN_GENERATION_PROMPT,
+        **kwargs: Any,
     ) -> GremlinQAChain:
         """Initialize from LLM."""
         qa_chain = LLMChain(llm=llm, prompt=qa_prompt)
         gremlin_generation_chain = LLMChain(llm=llm, prompt=gremlin_prompt)
-
+        gremlinl_fix_chain = LLMChain(llm=llm, prompt=gremlin_fix_prompt)
         return cls(
             qa_chain=qa_chain,
             gremlin_generation_chain=gremlin_generation_chain,
+            gremlin_fix_chain=gremlinl_fix_chain,
             **kwargs,
         )
 
     def _call(
-            self,
-            inputs: Dict[str, Any],
-            run_manager: Optional[CallbackManagerForChainRun] = None,
+        self,
+        inputs: Dict[str, Any],
+        run_manager: Optional[CallbackManagerForChainRun] = None,
     ) -> Dict[str, str]:
         """Generate gremlin statement, use it to look up in db and answer question."""
         _run_manager = run_manager or CallbackManagerForChainRun.get_noop_manager()
@@ -104,11 +114,13 @@ class GremlinQAChain(Chain):
 
         intermediate_steps: List = []
 
-        generated_gremlin = self.gremlin_generation_chain.invoke(
+        chain_response = self.gremlin_generation_chain.invoke(
             {"question": question, "schema": self.graph.get_schema}, callbacks=callbacks
         )
 
-        generated_gremlin = extract_gremlin(generated_gremlin["text"])
+        generated_gremlin = extract_gremlin(
+            chain_response[self.gremlin_generation_chain.output_key]
+        )
 
         _run_manager.on_text("Generated gremlin:", end="\n", verbose=self.verbose)
         _run_manager.on_text(
@@ -118,7 +130,9 @@ class GremlinQAChain(Chain):
         intermediate_steps.append({"query": generated_gremlin})
 
         if generated_gremlin:
-            context = self.graph.query(generated_gremlin)[: self.top_k]
+            context = self.execute_with_retry(
+                _run_manager, callbacks, generated_gremlin
+            )[: self.top_k]
         else:
             context = []
 
@@ -143,3 +157,62 @@ class GremlinQAChain(Chain):
             chain_result[INTERMEDIATE_STEPS_KEY] = intermediate_steps
 
         return chain_result
+
+    def execute_query(self, query: str) -> List[Any]:
+        try:
+            return self.graph.query(query)
+        except Exception as e:
+            if hasattr(e, "status_message"):
+                raise ValueError(e.status_message)
+            else:
+                raise ValueError(str(e))
+
+    def execute_with_retry(
+        self,
+        _run_manager: CallbackManagerForChainRun,
+        callbacks: CallbackManager,
+        generated_gremlin: str,
+    ) -> List[Any]:
+        try:
+            return self.execute_query(generated_gremlin)
+        except Exception as e:
+            retries = 0
+            error_message = str(e)
+            self.log_invalid_query(_run_manager, generated_gremlin, error_message)
+
+            while retries < self.max_fix_retries:
+                try:
+                    fix_chain_result = self.gremlin_fix_chain.invoke(
+                        {
+                            "error_message": error_message,
+                            # we are borrowing template from sparql
+                            "generated_sparql": generated_gremlin,
+                            "schema": self.schema,
+                        },
+                        callbacks=callbacks,
+                    )
+                    fixed_gremlin = fix_chain_result[self.gremlin_fix_chain.output_key]
+                    return self.execute_query(fixed_gremlin)
+                except Exception as e:
+                    retries += 1
+                    parse_exception = str(e)
+                    self.log_invalid_query(_run_manager, fixed_gremlin, parse_exception)
+
+        raise ValueError("The generated SPARQL query is invalid.")
+
+    def log_invalid_query(
+        self,
+        _run_manager: CallbackManagerForChainRun,
+        generated_query: str,
+        error_message: str,
+    ) -> None:
+        _run_manager.on_text("Invalid Gremlin query: ", end="\n", verbose=self.verbose)
+        _run_manager.on_text(
+            generated_query, color="red", end="\n", verbose=self.verbose
+        )
+        _run_manager.on_text(
+            "Gremlin Query Parse Error: ", end="\n", verbose=self.verbose
+        )
+        _run_manager.on_text(
+            error_message, color="red", end="\n\n", verbose=self.verbose
+        )
